8440ad1893dcf377c2693c77d5a26d7e2d3cb529
Richard Rowland <richardpietrowland@gmail.com>
2019-01-12 03:36:44 +0000

    Better comments and verbose messages

--- FilmStrip.ps1
+++ FilmStrip.ps1
@@ -12,6 +12,9 @@ Param (
     [switch] $KeepFrames
 )
 
+# Clear screen 
+Clear-Host
+
 # Enum for size specified
 enum Size {
     Tiny = 100;
@@ -36,9 +39,6 @@ Switch ($Orientation) {
     }
 }
 
-# Clear screen 
-Clear-Host
-
 # First test filepath given
 if (!(Test-Path -Path $VideoFile -PathType leaf)) {
     throw "Not a valid file"
@@ -48,13 +48,14 @@ if (!(Test-Path -Path $VideoFile -PathType leaf)) {
 $outputBasePath = Split-Path -Path $VideoFile -Parent
 $outputFileName = ((Split-Path -Path $VideoFile -Leaf).Split('.'))[0] + '.bmp'
 $outputFilePath = Join-Path -Path $outputBasePath -ChildPath $outputFileName
+Write-Verbose "Ouput image file will be save to $outputFilePath"
 
-# Remove any old temp directory and then make a new one
+# Create temporary folder if it doesn't exist already
 New-Item -ItemType Directory -Path $PSScriptRoot\TempScreenCaps -Force | Out-Null
+Write-Verbose "Created temporary file for frames at $PSScriptRoot\TempScreenCaps"
 
 # Get the total time in seconds of the video file
 $totalTime = [int](& $PSScriptRoot\binaries\ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $VideoFile)
-
 Write-Verbose "Creating final image measuring $($Width)x$($Height) for file $VideoFile which is $totalTime seconds long"
 
 # Create size variable
@@ -74,7 +75,7 @@ For ($i=0; $i -lt $Density; $i++) {
     & $PSScriptRoot\binaries\ffmpeg.exe -ss $currentTime -i $VideoFile -frames:v 3 -y $framePath 2>&1 | Out-Null
     Write-Verbose "Generated frame $i of $density at $currentTime seconds"
 
-    # Uses magick to extract the average colour of the frame to hex and stick in array
+    # Uses magick to extract the average colour of the frame to hex and format it for arg use
     $hexCode = (((& $PSScriptRoot\binaries\ImageMagick\magick.exe convert $framePath -resize 1x1 txt:-)[1]) -Split (' '))[3]
     $hexCodeArg = '"' + $hexCode + '"'
     Write-Verbose "Obtained average hex colour $hexCode for frame $i"
@@ -98,17 +99,19 @@ For ($i=0; $i -lt $Density; $i++) {
     & $PSScriptRoot\binaries\ImageMagick\magick.exe convert $outputFilePath -stroke none -fill $hexCodeArg -draw "rectangle $coord1 $coord2" $outputFilePath
     Write-Verbose "Added rectangle to main image from $coord1 to $coord2 using fill $hexCode"
 
-    # Write progress bar to console
+    # Update progress bar
     $percentComplete = (($i + 1) / $density) * 100
     Write-Progress -Activity 'Creating image' -Status "Frame $i of $density complete" -PercentComplete $percentComplete
 }
 
-# Wipes temp folder if specified
+# Wipes temp folder unless specified
 if (!($KeepFrames)) {
     Remove-Item -Path $PSScriptRoot\TempScreenCaps -Recurse -Force | Out-Null
+    Write-Verbose "Removed temporary frame files and folder"
 }
 
 # Displays final result
 & $outputFilePath
+Write-Verbose "Opening final image file from $outputFilePath"
 
 
